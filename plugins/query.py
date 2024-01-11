94537cb575aebf103bcbd7664185447c3f4bc87c
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-01-11 01:51:01 +0100

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -103,7 +103,7 @@ async def Cb_Handle(bot: Client, query: CallbackQuery):
         try:
             c_thumb = await db.get_thumbnail(query.from_user.id)
             ffmpeg = "-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"
-            await CompressVideo(bot=bot, query=query, ffmpegcode='ffmpeg', c_thumb=c_thumb)
+            await CompressVideo(bot=bot, query=query, ffmpegcode=ffmpeg, c_thumb=c_thumb)
 
         except Exception as e:
             print(e)
