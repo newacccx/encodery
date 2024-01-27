a9934014ee7f62894f593c5de4b06a8440cd4ac7
newacccx <119074899+newacccx@users.noreply.github.com>
2024-01-27 17:07:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "16768772")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "08d78fb05bdb90f1be4a4f1f0fef5f1e") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6392080677:AAF7neMmcfinPZuzqfRdbtPXcc_8TD9lhhY") # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "6381607")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "9799ad1623afe9bad664501f984b71fe") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6460534569:AAECzJE50Niw7wUMJiPM6kdKqPIy4WrfRaM") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
-    DB_NAME  = os.environ.get("DB_NAME","Roku") 
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://autofilter:autofilter@cluster0.nncrrgy.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
+    DB_NAME  = os.environ.get("DB_NAME","telegram") 
 
     # Other Configs 
-    ADMIN = int(os.environ.get("ADMIN", "1966867320")) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002020039437')) # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN", "1258695344")) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1001836524652')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
     START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
 
